2f14f06d81f0eb5071f7992a17ba5607fe07018e
Phil Jackson <phil@shellarchive.co.uk>
2012-03-19 14:51:18 +0000

    Unicode/utf8 support.

--- py_to_xml.py
+++ py_to_xml.py
@@ -1,17 +1,21 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from lxml import etree
 
 class PyToXml(object):
-    def __init__(self, root_name, structure):
+    def __init__(self, root_name, structure, encoding="UTF-8", xml_declaration=False):
         self.root = etree.Element(root_name)
         self.root_name = root_name
         self.structure = structure
+        self.encoding = encoding
+        self.xml_declaration = xml_declaration
     
     def pluralisation(self, plural):
         return "item"
 
     def traverse(self, structure, document, name):
-        if type(structure) == str:
+        if type(structure) in [ basestring, str, unicode ]:
             document.text = structure
 
         if type(structure) in [ int, float ]:
@@ -27,11 +31,13 @@ class PyToXml(object):
                 sub = etree.SubElement(document, key)
                 self.traverse(value, sub, key)
 
-    def to_xml(self):
+    def encode(self):
         return self.traverse(self.structure, self.root, self.root_name)
 
     def __str__(self):
-        return etree.tostring(self.root)
+        return etree.tostring(self.root,
+                              encoding=self.encoding,
+                              xml_declaration=self.xml_declaration)
 
 
 class TestPyToXml(unittest.TestCase):
@@ -45,29 +51,36 @@ class TestPyToXml(unittest.TestCase):
 
     def test_int_values(self):
         p2x = PyToXml("root", { "one": 2 })
-        p2x.to_xml()
+        p2x.encode()
         self.assertEqual(str(p2x), "<root><one>2</one></root>")
 
     def test_float_values(self):
         p2x = PyToXml("root", { "one": 2.3 })
-        p2x.to_xml()
+        p2x.encode()
         self.assertEqual(str(p2x), "<root><one>2.3</one></root>")
 
     def test_string_values(self):
         p2x = PyToXml("root", { "one": "two" })
-        p2x.to_xml()
+        p2x.encode()
         self.assertEqual(str(p2x), "<root><one>two</one></root>")
 
     def test_embedded_dict_values(self):
         p2x = PyToXml("root", { "a": { "b": "c" } })
-        p2x.to_xml()
+        p2x.encode()
         self.assertEqual(str(p2x), "<root><a><b>c</b></a></root>")
 
     def test_list_values(self):
         p2x = PyToXml("root", { "a": [1, 2] })
-        p2x.to_xml()
+        p2x.encode()
 
         output = "<root><a><item>1</item><item>2</item></a></root>"
+        self.assertEqual(str(p2x), output)
+
+    def test_unicode(self):
+        p2x = PyToXml("root", { "a": u"\u2603" })
+        p2x.encode()
+
+        output = "<root><a>☃</a></root>"
         self.assertEqual(str(p2x), output)
 
 if __name__ == '__main__':
