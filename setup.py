88813317cc01b396da7d62bbf2fd63712c65aae6
Phil Jackson <phil@shellarchive.co.uk>
2012-03-23 17:30:28 +0000

    Fail silently if the readme file can't be found.

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ import os
 from setuptools import setup
 
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+# Utility function to read the README file.  Used for the
+# long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to
+# put a raw string in below ...
+#
+# despite the manifest the readme.md will be missing when installed
+# from pypi, so we need to just fail silently here.
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except:
+        return ""
 
 setup(
     name="pyToXml",
